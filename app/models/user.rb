c9bf023b00780a908d30ece4507f31b2f4f0f25e
Nekoiii <10nekosa@gmail.com>
2023-08-03 13:47:58 +0900

    added some user validation test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-  
+  NAME_LENGTH_MIN = 1
+  NAME_LENGTH_MAX = 30
+
   has_many :user_records
   has_many :records, through: :user_records
   
-  validates :name, presence: true, length:{minimum:1, maximum:30}
+  validates :name, presence: true, length:{minimum:NAME_LENGTH_MIN, maximum:NAME_LENGTH_MAX}
+  validates :email, presence: false
 
 
   after_create :oncreate
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,13 +4,26 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(
-      name:"user_1_name"
+      name:"aaa"
     )
   end
 
   test "should be valid" do
     assert @user.valid?, @user.errors.full_messages
+    p 'aaaaa' if @user.valid?
   end
-
+  
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+  test "name should not be too long" do
+    @user.name = "a" * (User::NAME_LENGTH_MAX + 1)
+    assert_not @user.valid?   
+  end
+  test "name should not be too short" do
+    @user.name = "a" * (User::NAME_LENGTH_MIN - 1)
+    assert_not @user.valid?
+  end
 
 end
