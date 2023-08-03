c9a8c43c2b6ea6cb91a65dae31697199da6d4a99
Nekoiii <10nekosa@gmail.com>
2023-08-03 19:08:19 +0900

    doing email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,28 @@
 class User < ApplicationRecord
   NAME_LENGTH_MIN = 1
   NAME_LENGTH_MAX = 30
+  # regex for email: https://qiita.com/HIROKOBA/items/1358aa2e9652688698ee
+  EMAIL_LENGTH_MAX = 255
+  VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
 
   has_many :user_records
   has_many :records, through: :user_records
   
   validates :name, presence: true, length:{minimum:NAME_LENGTH_MIN, maximum:NAME_LENGTH_MAX}
-  validates :email, presence: false
+  validates :email, presence: false, length:{maximum:EMAIL_LENGTH_MAX},
+                                     format: { with: VALID_EMAIL_REGEX, allow_blank: true},
+                                     uniqueness: { case_sensitive: false, allow_blank: true}
 
+
+  before_save :prepare_for_save
+  def prepare_for_save
+    self.email = email.downcase
+  end
 
   after_create :oncreate
-
   def oncreate
-    puts 'Successfully created a new user'
+    puts "Successfully created a new user: " <<
+          "ID - #{self.id}, Name: #{self.name} ."
   end
 
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,7 +10,6 @@ class UserTest < ActiveSupport::TestCase
 
   test "should be valid" do
     assert @user.valid?, @user.errors.full_messages
-    p 'aaaaa' if @user.valid?
   end
   
   test "name should be present" do
@@ -24,6 +23,24 @@ class UserTest < ActiveSupport::TestCase
   test "name should not be too short" do
     @user.name = "a" * (User::NAME_LENGTH_MIN - 1)
     assert_not @user.valid?
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+  test "email addresses should be unique" do
+    return if @user.email.nil?
+    
+    duplicate_user = @user.dup
+    # (Because email addresses are not case sensitive ) 
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
   end
 
 end
