054730610830dddcda5b222dcc13d0b1cbf7bee4
Nekoiii <10nekosa@gmail.com>
2023-08-14 19:44:15 +0900

    continue doing login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # unprocessable_entity: Return 422 status code if the resource is not processable
-    render 'new', status: :unprocessable_entity
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      '''
+      reset_session: https://techtechmedia.com/session-rails/
+      セッションリプレイ攻撃（session replay attack）：https://techracho.bpsinc.jp/hachi8833/2023_06_02/130443 
+      セッションハイジャック( Session Hijacking)：https://www.ubsecure.jp/blog/session_hijacking 
+      セッション固定攻撃（session fixation）：https://railsguides.jp/security.html#%E3%82%BB%E3%83%83%E3%82%B7%E3%83%A7%E3%83%B3%E5%9B%BA%E5%AE%9A%E6%94%BB%E6%92%83 
+      '''
+      reset_session  
+       
+      log_in user
+      redirect_to user
+    else    
+      # flash v.s flash.now: https://qiita.com/jackie0922youhei/items/405711d2a2f1c483cbf9
+      flash.now[:danger] = 'Invalid email/password combination !'
+      # unprocessable_entity: Return 422 status code if the resource is not processable
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def destroy
